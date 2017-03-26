03f04f2bc9bded7e96ce9e7eb76ee3f5bc22505f
Juan Facorro <juan.facorro@klarna.com>
2017-03-26 10:56:02 +0200

    Discover coverdata files

--- src/cover_result_prv.erl
+++ src/cover_result_prv.erl
@@ -24,7 +24,7 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    report(),
+    report(State),
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
@@ -35,51 +35,52 @@ format_error(Reason) ->
 %% Internal functions
 %% ===================================================================
 
-report() ->
-  CoverSpecPath = case os:getenv("COVER_SPEC") of
-                    false ->
-                      throw("COVER_SPEC environment variable missing.");
-                    Value ->
-                      Value
-                  end,
-  {ok, CoverSpec} = file:consult(CoverSpecPath),
-  CoverDataPath = case proplists:get_value(export, CoverSpec) of
-                    undefined ->
-                      throw("No 'export' config in Cover specifications.");
-                    X -> X
-                    end,
+report(State) ->
+    CoverDir = cover_dir(State),
+    CoverFiles = get_all_coverdata(CoverDir),
+    cover:import(CoverFiles),
 
-  file:set_cwd(filename:dirname(CoverSpecPath)),
-  cover:import(CoverDataPath),
+    {result, Ok, Fail} = cover:analyze(module),
 
-  {result, Ok, Fail} = cover:analyze(module),
+    io:format("~n~s==== Code Coverage ====~s~n~n",
+              [color("white-bold"), color("reset")]),
+    [report_result(Result) || Result <- lists:sort(Ok)],
 
-  io:format("~n~s==== Code Coverage ====~s~n~n",
-            [color("white-bold"), color("reset")]),
-  [report_result(Result) || Result <- lists:sort(Ok)],
+    case Fail of
+        [] -> ok;
+        _ ->
+            io:format("~nCoverage analysis failed in the following modules: "),
+            [io:format("~p, ", [Module]) || {_, Module} <- lists:sort(Fail)]
+    end,
 
-  case Fail of
-    [] -> ok;
-    _ ->
-      io:format("~nCoverage analysis failed in the following modules: "),
-      [io:format("~p, ", [Module]) || {_, Module} <- lists:sort(Fail)]
-  end,
+    io:format("~n~s=======================~s~n~n",
+              [color("white-bold"), color("reset")]).
 
-  io:format("~n~s=======================~s~n~n",
-            [color("white-bold"), color("reset")]).
+cover_dir(State) ->
+    filename:join([rebar_dir:base_dir(State), "cover"]).
+
+get_all_coverdata(CoverDir) ->
+    ok = filelib:ensure_dir(filename:join([CoverDir, "dummy.log"])),
+    {ok, Files} = rebar_utils:list_dir(CoverDir),
+    rebar_utils:filtermap(fun(FileName) ->
+        case filename:extension(FileName) == ".coverdata" of
+            true  -> {true, filename:join([CoverDir, FileName])};
+            false -> false
+        end
+    end, Files).
 
 report_result({Module, {0, 0}}) ->
-  report_result({Module, {1, 0}});
+    report_result({Module, {1, 0}});
 report_result({Module, {Cov, NotCov}}) ->
-  Coverage = erlang:round(100 * Cov / (Cov + NotCov)),
-  Color = get_color(Coverage),
-  Reset = color("reset"),
-  io:format("[~s% ~3.. B~s]\t~p~n", [Color, Coverage, Reset, Module]).
+    Coverage = erlang:round(100 * Cov / (Cov + NotCov)),
+    Color = get_color(Coverage),
+    Reset = color("reset"),
+    io:format("[~s% ~3.. B~s]\t~p~n", [Color, Coverage, Reset, Module]).
 
 get_color(Coverage) when Coverage == 100 ->
-  color("green-bold");
+    color("green-bold");
 get_color(Coverage) when Coverage > 90 ->
-  color("green");
+    color("green");
 get_color(Coverage) when Coverage > 70 ->
     color("yellow");
 get_color(Coverage) when Coverage > 50 ->
@@ -88,12 +89,12 @@ get_color(_Coverage) ->
     color("red").
 
 color(Name) ->
-  Colors = #{"red" => "\e[0;31m",
-             "red-bold" => "\e[1;31m",
-             "green" => "\e[0;32m",
-             "green-bold" => "\e[1;32m",
-             "yellow" => "\e[1;33m",
-             "white" => "\e[0;37m",
-             "white-bold" => "\e[1;37m",
-             "reset" => "\e[0m"},
-  maps:get(Name, Colors).
+    Colors = #{"red" => "\e[0;31m",
+               "red-bold" => "\e[1;31m",
+               "green" => "\e[0;32m",
+               "green-bold" => "\e[1;32m",
+               "yellow" => "\e[1;33m",
+               "white" => "\e[0;37m",
+               "white-bold" => "\e[1;37m",
+               "reset" => "\e[0m"},
+    maps:get(Name, Colors).
